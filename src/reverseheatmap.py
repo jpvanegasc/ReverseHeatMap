5b441cbfcf8eab7b29423588f3916c25c302b4da
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2021-04-16 10:59:20 -0500

    Feat: python3 and minor modifications

--- src/reverseheatmap.py
+++ src/reverseheatmap.py
@@ -1,41 +1,48 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 import sys,os,math
 from PIL import Image
 
 # open the image and determine size.
 im = Image.open("heatmap.png")
-x = 0
-y = 0
+
 size = im.size
 x_max = size[0]
 y_max = size[1]
-print x_max
-print y_max
-list = []
+
+print(f"Image is: {x_max} by {y_max} pixels")
+
+lis = []
+x = 0
+y = 0
 
 while (x < x_max):
-	while (y < y_max):
-		list.append([x,y,im.getpixel((x,y))])
-		y+=1
-	x+=1
-	y=1
-os.remove('results.csv’)
-appender = open('results.csv','ab')
-appender.write("x,y,r,g,b,rgbtotal\r\n")
+    while (y < y_max):
+        lis.append([x,y,im.getpixel((x,y))])
+        y+=1
+
+    x+=1
+    y = 0
+
+os.remove('results.csv')
+file = open('results.csv','w+')
+file.write("x,y,r,g,b,rgbtotal\r\n")
 result = []
-for each in list:
-	x = str(each[0]).zfill(2)
-	y = str((each[1]*-1)+99).zfill(2)
-	rgb = str(each[2]).split(",")
-	r = rgb[0].replace("(","")
-	g = rgb[1].replace(" ","")
-	b = rgb[2].replace(" ","").replace(")","")
-	result.append([x,y,r,g,b])
+
+for each in lis:
+    x = each[0]
+    y = each[1]
+    rgb = str(each[2]).split(",")
+    r = rgb[0].replace("(","")
+    g = rgb[1].replace(" ","")
+    b = rgb[2].replace(" ","").replace(")","")
+    result.append([x,y,r,g,b])
+
 result = sorted(result)
 
 for each in result:
-	total = str(int(each[1])+int(each[2])+int(each[3]))
-	writeme = str(each[0]+','+each[1]+','+each[2]+','+each[3]+','+total+'\r\n')
-	appender.write(writeme)
+    total = int(each[2]) + int(each[3]) + int(each[4])
+    writeme = str(str(each[0]) + ',' + str(each[1]) + ',' + str(total) + '\n')
+
+    file.write(writeme)
+
